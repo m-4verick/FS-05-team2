4879955916646882a967fab80e0655fac36376c4
m-4verick <theodoreshin99@icloud.com>
2024-12-13 16:40:12 +0900

    init: 신진호 최종

--- 신진호.js
+++ 신진호.js
@@ -1,4 +1,15 @@
 const deploy = document.querySelector("deploy");
+const loginBtn = document.querySelector(".loginBtn");
+
+
+document.querySelector('.loginBtn').addEventListener('click', () => {
+    const www = document.querySelector('.www');
+    const login = document.querySelector('.login');
+
+    login.style.display = 'none';
+    www.style.display = 'block';
+});
+
 
 document.querySelector('.deploy').addEventListener('click', () => {
     const easterEgg = document.querySelector('.easterEGG');
