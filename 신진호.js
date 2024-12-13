0c7f3893a447a15cc66a4a8f564e4d7dac43efeb
m-4verick <theodoreshin99@icloud.com>
2024-12-13 15:24:02 +0900

    init : 신진호 통합본

---
+++ 신진호.js
@@ -0,0 +1,6 @@
+const deploy = document.querySelector("deploy");
+
+document.querySelector('.deploy').addEventListener('click', () => {
+    const easterEgg = document.querySelector('.easterEGG');
+    easterEgg.style.display = 'block';
+});
